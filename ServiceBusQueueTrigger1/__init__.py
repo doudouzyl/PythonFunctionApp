28533f0e08c0819e2ccc0300042de281fff24a4f
doudouzyl <doudouzyl@gmail.com>
2023-05-26 22:17:56 +1000

    Update __init__.py

--- ServiceBusQueueTrigger1/__init__.py
+++ ServiceBusQueueTrigger1/__init__.py
@@ -2,6 +2,12 @@ import logging
 
 import azure.functions as func
 
+from azure.servicebus import ServiceBusClient
+from azure.servicebus.amqp import AmqpAnnotatedMessage, AmqpMessageBodyType
+import requests
+import uamqp
+from uamqp import types as uamqp_types, utils, authentication, constants
+
 
 def main(msg: func.ServiceBusMessage):
     logging.info('Python ServiceBus queue trigger processed message: %s',
